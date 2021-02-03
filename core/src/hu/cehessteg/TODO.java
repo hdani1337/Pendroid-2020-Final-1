472743c5ba0fa6734853fa5b8ee3c5000c9f4ea1
Horváth Dániel <admin@hdani1337.hu>
2021-02-03 20:12:23 +0100

    Maradék teendő, ami annyit takar hogy iszok még egy fröccsöt

--- core/src/hu/cehessteg/TODO.java
+++ core/src/hu/cehessteg/TODO.java
@@ -2,14 +2,14 @@ package hu.cehessteg;
 
 public class TODO {
     /**
-     * Játékmechanizmus kész
-     * Játékossal való interakció a játékmenetben kész
-     * TODO Bence megcsinálja a textúrákat (menügombok, háttér, játék logo, blokkok, egyéb gombok)
-     * TODO Ezen textúrák importálása kódszinten
-     * TODO Játékos rekordjának elmentése az eszközre, játékmenetenként ezt kijelezni
-     * TODO Nehézségi fokozatok
-     * TODO Loopolható zenét lopni netről, s ehhez némítási opció a beállításokban
-     *
+     * KÉSZ Játékmechanizmus
+     * KÉSZ Játékossal való interakció a játékmenetben
+     * KÉSZ Bence megcsinálja a textúrákat (menügombok, háttér, játék logo, blokkok, egyéb gombok)
+     * KÉSZ Ezen textúrák importálása kódszinten
+     * KÉSZ Játékos rekordjának elmentése az eszközre, játékmenetenként ezt kijelezni
+     * KÉSZ Nehézségi fokozatok
+     * KÉSZ Loopolható zenét lopni netről, s ehhez némítási opció a beállításokban
+     * TODO Kódtakarítás
      * Mást meg csináljon az akinek két anyja van, ennyi bőven elég lesz
      * **/
 }
